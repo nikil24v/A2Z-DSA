edf0ae64fdac408a167e53ef42be1b688264e04b
Nikil <nikil24v@gmail.com>
2026-04-06 22:04:47 +0530

    Added peak elemnt problem

--- arrays/striver-bs/striver13.cpp
+++ arrays/striver-bs/striver13.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int main() //Find the peak element
 {
     vector<int> nums = {1,2,3,1};
+    int peak = -1;
 
-    
+    for(int i=0;i<nums.size();i++)
+    {
+        if(i > 0 && i < nums.size())
+        {
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1])
+            {
+                peak = nums[i];
+            }
+        }
+    }
+
+    cout<<"The peak elemnt is "<<peak<<endl;
 }
