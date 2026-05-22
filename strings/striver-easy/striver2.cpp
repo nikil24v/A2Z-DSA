9fc06e6f6fc3d570fee294698c49506e5d4714f0
Nikil <nikil24v@gmail.com>
2026-05-22 11:45:58 +0530

    Reverse the string was added

--- strings/striver-easy/striver2.cpp
+++ strings/striver-easy/striver2.cpp
@@ -3,7 +3,47 @@ using namespace std;
 
 int main()  //Reverse the string 
 {
-    string s = "the sky is blue";
+    string s = "the sky is   blue  ";
 
-    
+    if(s.empty() == true)
+    {
+        cout<<"The String is empty"<<endl;
+        return 0;
+    }
+    else
+    {
+        while(s.front() == ' ')
+        {
+            s.erase(0,1);
+        }
+
+        while(!s.empty() && s.back() == ' ')
+        {
+            s.pop_back();
+        }
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i] == ' ' && s[i+1] == ' ')
+            {
+                s.erase(i,1);
+                i--;
+            }
+        }
+    }
+
+    cout<<s<<endl;
+    reverse(s.begin(),s.end());
+
+    int start = 0;
+
+    for(int i=0;i<=(int)s.length();i++)
+    {
+        if(i == s.length() || s[i] == ' ')
+        {
+            reverse(s.begin() + start , s.begin() + i);
+            start = i+1;
+        }
+    }
+    cout<<s<<endl;
 }
